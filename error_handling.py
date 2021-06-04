a260a6f913f7d63623eb9271398b80eddc990d66
Mikayla Beelders <mikaylabeelders@gmail.com>
2021-06-04 09:14:52 +0200

    New changes.

--- error_handling.py
+++ error_handling.py
@@ -1,14 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
 window = Tk()
-window.geometry("400x300")
+window.geometry("500x300")
 window.title("Error Handling:")
 window.config(bg="#2e4053")
 # label and entry
-amount_label = Label(window, fg="white", bg="#2e4053", text="Please enter the amount in your account")
-amount_label.place(relx=0.16, rely=0.2)
+amount_label = Label(window, fg="white", bg="#2e4053", text="Please enter the amount in your account:", font=("Helvetica", 15))
+amount_label.place(relx=0.22, rely=0.2)
 amount_entry = Entry(window)
-amount_entry.place(relx=0.28, rely=0.35)
+amount_entry.place(relx=0.34, rely=0.35)
 # function
 def check_funds():
     amount = amount_entry.get()
@@ -17,10 +17,10 @@ def check_funds():
         if amount >= 3000:
             messagebox.showinfo("Status Feedback", "Congratulations:\nYou qualify for Malaysia!")
         else:
-            messagebox.showinfo("Status Feedback", "Please deposit more funds for the excursion")
+            messagebox.showinfo("Status Feedback", "Please deposit more funds for the excursion.")
     except ValueError:
         messagebox.showerror("Error", "Please enter a valid input")
 # check button
-check_btn = Button(window, text="Check Amount", bg="#2e4053", fg="white", borderwidth=5, padx=15, pady=10, command=check_funds)
-check_btn.place(relx=0.31, rely=0.5)
+check_btn = Button(window, text="Check Amount", bg="#2e4053", fg="white", borderwidth=5, padx=15, pady=10, command=check_funds, font=("Helvetica", 15))
+check_btn.place(relx=0.37, rely=0.5)
 window.mainloop()
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from tkinter import *
 from tkinter import messagebox
 # tkinter body
 root = Tk()
-root.geometry("400x300")
+root.geometry("500x300")
 root.title("Authentification")
 root.config(bg="#34495e")
 # heading
 heading = Label(root, fg="white", bg="#34495e", text="Please enter your login credentials:", font=("Helvetica", 15, "bold"))
-heading.place(relx=0.08, rely=0.14)
+heading.place(relx=0.17, rely=0.14)
 # frame
 frame = Frame(root,bg="#34495e")
-frame.place(relx=0.3, rely=0.3)
+frame.place(relx=0.33, rely=0.3)
 # username label and entry
 username = Label(frame, bg="#34495e", fg="white", text="Username:", font=("Helvetica", 11))
 username.pack()
@@ -31,11 +31,11 @@ def login():
             root.destroy()
             import error_handling
         else:
-            messagebox.showerror("Login Details","Please enter valid login credentials" )
+            messagebox.showerror("Login Details","Please enter valid login credentials." )
             user_entry.delete(0, END)
             password_entry.delete(0, END)
 # login button
 login_btn = Button(root, borderwidth=3, text="Login", padx=15, pady=10, bg="#2e4053", fg="white", command=login, font=("Helvetica", 10, "bold"))
-login_btn.place(relx=0.41, rely=0.7)
+login_btn.place(relx=0.42, rely=0.7)
 
 root.mainloop()
